c10e2e9824132e0ca92b8ecf6548402abe5983a7
wangxiaomei <lizhigao521123@163.com>
2023-11-11 08:14:21 +0800

    构建了PRPC的服务发现注册模块的主体框架，待继续完善和测试

--- common/prpc/discov/discovery.go
+++ common/prpc/discov/discovery.go
@@ -8,6 +8,7 @@ type Discovery interface {
 	NotifyListeners()
 	AddNotify(f func())
 	Name() string
-	RegisterService(ctx context.Context, service *service)
-	UnRegisterService(ctx context.Context, service *service)
+	RegisterService(ctx context.Context, service *Service)
+	UnRegisterService(ctx context.Context, service *Service)
+	GetService(ctx context.Context, serviceName string) *Service
 }
