018a26ea5d2064c7ad3d777553830dcdff1694ad
wangxiaomei <lizhigao521123@163.com>
2023-11-11 17:46:00 +0800

    初步完成了PRPC的服务注册发现模块，并实现了相应暴露出去的接口，还需要进一步测试保证该模块的可用性

---
+++ common/prpc/discov/plugin/plugin.go
@@ -0,0 +1,24 @@
+package plugin
+
+import (
+	"context"
+	"fmt"
+	"im/common/config"
+	"im/common/prpc/discov"
+	"im/common/prpc/discov/etcd"
+)
+
+func GetDiscoInstance(ctx context.Context) (discov.Discovery, error) {
+	/*判断这个使用了什么作为注册中心*/
+	discovName := config.GetPrpcDiscovName()
+	switch discovName {
+	case "etcd":
+		{
+			if !etcd.IsEtcdDiscoveryInit() {
+				etcd.RunMainRegister(ctx, config.GetEtcdEndPointsForDiscovery())
+			}
+			return etcd.GetEtcdDiscoveryInstance(), nil
+		}
+	}
+	return nil, fmt.Errorf("获取注册中心实例失败！")
+}
